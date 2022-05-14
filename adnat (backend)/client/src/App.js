1b38383642eebb0604b5c283684fb77a207b7ad1
gehrig_barnes <gehrigbarnes@gmail.com>
2022-05-14 13:05:44 -0500

    created a useEffect that will store the user as state when the user logs in. Also created a funciton that sets the user state to null when the user logs out

--- adnat (backend)/client/src/App.js
+++ adnat (backend)/client/src/App.js
@@ -3,6 +3,31 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 
 
 function App() {
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  function handleLogOutClick(){
+    fetch("/logout",{
+        method: "DELETE"
+    }).then((r) => {
+        if(r.ok){
+            setUser(null);
+        }
+    });
+    // Navigate to home page after logout and clear history
+    navigate("/");
+}
+
+
   return (
     <div className="App">
       hello world
