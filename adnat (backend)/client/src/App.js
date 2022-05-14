180fc5b570d837c8c83d4976eaf312d72568b24e
gehrig_barnes <gehrigbarnes@gmail.com>
2022-05-14 13:00:59 -0500

    setting up a simple front end using javascript

---
+++ adnat (backend)/client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
